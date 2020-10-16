c8a878103f4f4c73a1f700256e933b30062be569
rakesh mishra <rakesh.e.mishra@accenture.com>
2020-10-16 14:24:19 +0530

    Intial setup

---
+++ TestProject/TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by rakesh.e.mishra on 16/10/20.
+//  Copyright © 2020 rakesh.e.mishra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
