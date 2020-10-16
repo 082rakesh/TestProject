4d16247de4dcd8a510543658e3bd60caf13c4333
rakesh mishra <rakesh.e.mishra@accenture.com>
2020-10-16 14:37:59 +0530

    reverted both print

--- TestProject/TestProject/ViewController.swift
+++ TestProject/TestProject/ViewController.swift
@@ -14,13 +14,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("viewDidLoad")
-        
-        print("viewWillAppear")
-        
-        print("viewWDidAppear")
-
-
-        
     }
 
 
